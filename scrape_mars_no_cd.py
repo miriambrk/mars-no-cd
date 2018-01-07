39db9619a0b91fbe797b60515738871c5fa9fef3
Miriam A Berkowitz <miriambrk@gmail.com>
2018-01-06 23:33:09 -0500

    try with firefox

--- scrape_mars_no_cd.py
+++ scrape_mars_no_cd.py
@@ -8,7 +8,7 @@ import os
 
 def scrape():
 
-    browser = Browser('chrome', headless=True)
+    browser = Browser('firefox', headless=False)
 
     #store all the scraped data in a dictionary
     mars_dictionary = {}
